135406ee514232a5ed898e951157114d96338b68
Ubuntu <ubuntu@ip-172-31-19-161.us-west-1.compute.internal>
2021-09-05 01:32:49 +0000

    final

--- data/helper.py
+++ data/helper.py
@@ -9,15 +9,15 @@ from dotenv import load_dotenv
 logging.basicConfig(level=logging.INFO)
 load_dotenv()
 bot = commands.Bot(command_prefix='$')
-ffmpegLib = os.fspath("ffmpeg\\bin\\ffmpeg")
+ffmpegLib = os.fspath("ffmpeg/bin/ffmpeg")
 
 
 def bonk_file():
-    return discord.FFmpegPCMAudio(source=Path('bonker\\bonk.mp3'), executable=ffmpegLib)
+    return discord.FFmpegPCMAudio(source=Path('bonker/bonk.mp3'))#, executable=ffmpegLib)
 
 
 def chasing():
-    return discord.FFmpegPCMAudio(source=Path('bonker\\bonk.mp3'), executable=ffmpegLib)
+    return discord.FFmpegPCMAudio(source=Path('bonker/bonk.mp3'), executable=ffmpegLib)
 
 
 token = os.getenv('TOKEN')
